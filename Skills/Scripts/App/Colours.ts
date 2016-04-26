385768c00dbe5f9ea7bc73d2b130110e0eb2b8fd
Chester Burbidge <chester.burbidge@abide-financial.com>
2016-04-26 13:08:14 +0100

    Added stuff

---
+++ Skills/Scripts/App/Colours.ts
@@ -0,0 +1,29 @@
+﻿/// <reference path='_references.ts' />
+
+module App
+{
+	export interface IRandomiser
+	{
+		mix():string;
+	}
+	
+	export class ColourRandomiser implements IRandomiser
+	{
+		constructor(public colours:string[]){}
+		
+		mix(){
+			var index = Math.floor(Math.random() * this.colours.length); 
+			return this.colours[index];
+		}
+	}
+	
+	export class Colours 
+	{
+		constructor(public cVData:CV.ICVData, public mixer:IRandomiser)
+		{
+			
+		}
+		
+		
+	}
+}
--- Skills/Scripts/App/_external_references.ts
+++ Skills/Scripts/App/_external_references.ts
@@ -1,2 +1,3 @@
 ﻿/// <reference path='IdAndActive.ts' />
 /// <reference path='LengthScaler.ts' />
+/// <reference path='Colours.ts' />
